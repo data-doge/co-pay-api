1767e7871fec84edddfebae2a41e20e818fc7f2a
euglazer <eugenestapler@gmail.com>
2015-09-20 12:16:41 -0700

    add generic user acct to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-require 'faker'
+require "faker"
 
 Group.destroy_all
 Membership.destroy_all
@@ -7,5 +7,9 @@ User.destroy_all
 group = Group.create(name: Faker::Company.name)
 puts "created a group"
 
-6.times { group.users << User.create(name: Faker::Name.name, email: Faker::Internet.email, password: 'password') }
+user = User.create(name: "User", email: "user@example.com", password: "password")
+group.users << user
+puts "created generic user account"
+
+6.times { group.users << User.create(name: Faker::Name.name, email: Faker::Internet.email, password: "password") }
 puts "generated six memberships for group"
