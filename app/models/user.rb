dd8007f0d4fc9e76c9e89bf28b56c5472f15aac7
euglazer <eugenestapler@gmail.com>
2015-09-19 17:30:39 -0700

    devise_token_auth config and set up group and membership models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,17 @@
 class User < ActiveRecord::Base
-  # Include default devise modules.
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable,
           :confirmable, :omniauthable
   include DeviseTokenAuth::Concerns::User
+
+  before_validation :assign_uid_and_provider
+  
+  has_many :memberships
+  has_many :groups, through: :memberships
+
+  private
+    def assign_uid_and_provider
+      self.uid = self.email
+      self.provider = 'email'
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,11 @@
 require 'faker'
 
-6.times { User.create(name: Faker::Name.name, email: Faker::Internet.email, password: 'password') }
-puts "generated six users"
+Group.destroy_all
+Membership.destroy_all
+User.destroy_all
 
+group = Group.create(name: Faker::Company.name)
+puts "created a group"
+
+6.times { group.users << User.create(name: Faker::Name.name, email: Faker::Internet.email, password: 'password') }
+puts "generated six memberships for group"
